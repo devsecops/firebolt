04c4701653b78a25b44ea4121b6835a10128cda7
Graham Thomas <grahamthethomas@gmail.com>
2016-07-20 16:47:20 -0700

    Added functionality and edit .attack file to reflect

--- gauntlt/attack_adapters/metasploit.rb
+++ gauntlt/attack_adapters/metasploit.rb
@@ -3,5 +3,20 @@ When /^"metasploit" is installed$/ do
 end
 
 When /^I launch (?:a|an) "metasploit" attack with:$/ do |command|
+  if command.include? "exit"
+  	run_with_profile command
+  else  
+  	run_with_profile command + " exit;"
+  end
+end
+
+When /^I launch (?:a|an) "metasploit" vulnerability check with:$/ do |command|
+  if !(command.include? "check")
+  	command += "check;"
+  end
+  if !(command.include? "exit")
+  	command += "exit;"
+  end
   run_with_profile command
 end
+
